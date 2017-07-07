5de9ff725f9f713a21f270b75e46bc54c7136f4b
kostyak76 <kostyak7676@gmail.com>
2017-07-07 19:32:01 -0400

    Update index.ts
    changed import so that 'map' method should be visible to Observable

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SwapiService {
